c5505995d175063f9277622f7f90ca7af826c0b7
MrPeppermint <jobradovic8@gmail.com>
2020-05-29 23:09:05 +0200

    done with counting substrings(words)

--- typingTest.cpp
+++ typingTest.cpp
@@ -17,40 +17,93 @@
 */
 
 
-struct Timer {
-    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-    std::chrono::duration<float> userDuration, givenDuration;
 
-    Timer(){
-        start = std::chrono::high_resolution_clock::now();
-    }
-
-    ~Timer(){
-        end = std::chrono::high_resolution_clock::now();
-        userDuration = end - start;
-
-        givenDuration += std::chrono::seconds(60);        
-        int userSpeed = userDuration.count();
-        std::cout << "I need to make this into a constant of 60s, and count how many words user got right";
-    }
-};
 
 class Speed {
 private:
     std::string name; //user name
     int score = 20; //user stat
     std::string text; //text for a user
+
+    /*struct Timer {
+        std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+        std::chrono::duration<float> userDuration, givenDuration;
+
+        Timer(){
+            start = std::chrono::high_resolution_clock::now();
+        }
+
+        ~Timer(){
+            end = std::chrono::high_resolution_clock::now();
+            userDuration = end - start;
+
+            givenDuration += std::chrono::seconds(60);        
+            int userSpeed = userDuration.count();
+            int givenTime = givenDuration.count();
+            int countScore = 0;
+            std::string word;
+            if(userSpeed != givenTime && word == wordText)
+                countScore++;
+            else
+                return;
+            //std::cout << userSpeed << "\n";
+            //std::cout << givenTime << "\n";
+        }
+    };
+*/
 public:
     Speed(std::string n, int s){
         name = n;
         score = s;
     }
+    #define out 0
+    #define in 1
+    int countSubstring(std::string text){
+        int count = 0; 
+        //int out = 0, in = 1;
+        int state = out;
+        int charCount = 0;
 
+        for(std::string::iterator it = text.begin(); it != text.end(); ++it){
+            
+            if(state == out){
+                state = in;
+                ++count;
+            }
+            else 
+                if(text[charCount] == ' ' || text[charCount] == '\n' || text[charCount] == '\t')
+                    state = out;
+            charCount++;
+                
+        }
+        
+        return count;
+    }
+
+    void startSpeed(){
+        std::chrono::duration<float> startDuration, givenDuration;
+        startDuration += std::chrono::seconds(0);
+        givenDuration += std::chrono::seconds(60);
+        int startSpeed = startDuration.count();
+        int givenTime = givenDuration.count();
+
+        int countScore = 0;
+        
+        for(startSpeed; startSpeed < givenTime; startSpeed++)
+            if(startSpeed != givenTime) //add if a substring is equal too
+                countScore++; 
+            else
+                return;
+        
+        
+        
+    }
     void checkSpeed(std::string text) {
-        std::cout << "Please start typing: \n";
+    
+        std::cout << name << ", please start typing whenever you feel like it: \n";
         std::cin >> this -> text;
 
-        if(this -> text == text){ //if user got one word right, FIX this later
+         //if user got one word right, FIX this later
             if(score >= 20 && score < 50){
                 std::cout << "Congratulations " << name << "! Your score is " << score
                 << "! That is 20 or more so you passed the beginner test! Shine On You Crazy Diamond!\n";
@@ -71,18 +124,15 @@ public:
                             << "! Hey you! What if you could make Money with these skills? Congratulation on 90 or more words per minute! You are a real deal!\n";
                         }
                         else{
-                            std::cout << "Sorry. I'm lost for words. I hope Eclipse hasn't swallowed you yet! Prepare yourself for next time, this time your score is under 20 :(\n";
+                            std::cout << "Sorry " << name << ". I'm lost for words. Your score is" << score << "I hope Eclipse hasn't swallowed you yet! Prepare yourself for next time, this time your score is under 20 :(\n";
                         }
-        }
-        else{
-            std::cout << "sorry little one\n";
-        }
     }
+    
 };
 
 int main(){
     std::string name;
-    int score = 10;
+    int score = 50;
 
     /*std::string text = "Remember when you were young, you shone like the sun\
                         Shine on you crazy diamond\
@@ -101,11 +151,12 @@ int main(){
                         Come on you raver, you seer of visions\
                         Come on you painter, you piper, you prisoner, and shine";
     */
-    std::string text = "NO";
-    Speed obj(name, score);
+    std::string text = "No you dumbass";
     std::cout << "Input your name, master\n";
     std::cin >> name;
-    obj.checkSpeed(text);   
+    Speed obj(name, score);
+    //obj.checkSpeed(text);   
+    std::cout << obj.countSubstring(text);
     /*Whole system of a down(ha ha ha), system of scoring, could be put in function later*/
     
 

d7a1fe804dc981bbc78bcdfd12f776b8f269676c
MrPeppermint <jobradovic8@gmail.com>
2020-06-02 14:15:25 +0200

    Done with counting(scoring) look at the top code for next

--- typingTest.cpp
+++ typingTest.cpp
@@ -2,76 +2,66 @@
 #include <chrono>
 
 /*
-    1. Make a timer that will return on 1minute(60 seconds)
-    2. When a word is written correctly, score++
-    3. https://www.youtube.com/watch?v=oEx5vGNFrLk
+    IDEAS:
+        1. Make given text appear slowly, or tell the user that they need to start in next 5 seconds or make it so the text appears
+           just before the "Start Typing" and give the user message "You have 10 seconds to start:"
+        2. Make three more levels: intermidate, hard, hardcore. Every stage has it's own Pink Floyd song, and different scoring statements
+        3. Arrange the code to look cleaner, maybe put the class in different file
+        4. Make an easter egg if a user has any of the names of the band members, send them a cool message or something
+        5. Try to use SQl and mySQL and try to save the names as data of users, there is no need for password IMHO
+        6. Make a visual version of this console program in SFML
+        7. Make the timer work as it should
+        8. Add so the text has a threshold of coming out, so the user can see just the portion of it, or make it so the user can see
+           only this and next word as they type. I see this much easier done with SFML since it can be visually done, but this should
+           be left as an end goal for a console version of this app
 
 */
-
-/*
-    1. 
-    2.
-    3.
-    4.
-
-*/
-
-
-
 
 class Speed {
 private:
     std::string name; //user name
-    int score = 20; //user stat
-    std::string text; //text for a user
-
-    /*struct Timer {
-        std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-        std::chrono::duration<float> userDuration, givenDuration;
-
-        Timer(){
-            start = std::chrono::high_resolution_clock::now();
-        }
-
-        ~Timer(){
-            end = std::chrono::high_resolution_clock::now();
-            userDuration = end - start;
-
-            givenDuration += std::chrono::seconds(60);        
-            int userSpeed = userDuration.count();
-            int givenTime = givenDuration.count();
-            int countScore = 0;
-            std::string word;
-            if(userSpeed != givenTime && word == wordText)
-                countScore++;
-            else
-                return;
-            //std::cout << userSpeed << "\n";
-            //std::cout << givenTime << "\n";
-        }
-    };
-*/
+    int score;
+    std::string givenTextEasy;
+    std::string userText; //text for a user
 public:
-    Speed(std::string n, int s){
+    Speed(std::string n){
         name = n;
-        score = s;
+        score = 0;
+        givenTextEasy = "Remember when you were young, you shone like the sun, Shine on you crazy diamond, Now there's a look in your eyes, like black holes in the sky, Shine on you crazy diamond, You were caught on the crossfire of childhood and stardom, Blown on the steel breeze, Come on you target for faraway laughter, Come on you stranger, you legend, you martyr, and shine, You reached for the secret too soon, you cried for the moon, Shine on you crazy diamond, Threatened by shadows at night, and exposed in the light, Shine on you crazy diamond, Well you wore out your welcome with random precision, Rode on the steel breeze, Come on you raver, you seer of visions, Come on you painter, you piper, you prisoner, and shine\n";
     }
 
-    int countSubstring(std::string text){
-        int count = 0; 
-        int out = 0, in = 1;
-        int state = out;
-        
-        for(std::string::iterator it = text.begin(); it != text.end(); ++it){
-            if(state == out){
-                state = in;
-                ++count;
+    bool compareChar(char first, char second){
+        if(first == second)
+            return true;
+        else
+            return false;
+    }
+
+    bool compareSubstrChar(char first, char second){
+        if(std::isspace(first) && std::isspace(second))
+            return true;
+        else  
+            return false;
+    }
+
+    int countSubstring(){
+        std::cout << givenTextEasy << "\n";
+        std::cout << "Start typing: \n";
+        std::getline(std::cin, userText);
+    
+        int i, j;
+        for(i = 0, j = 0; i < givenTextEasy.length() && j < userText.length(); ++i, ++j){
+            if(compareChar(givenTextEasy[i], userText[j])){
+                if(std::isspace(givenTextEasy[i + 1]) || std::isspace(userText[j + 1]))
+                    score++;
+                
+                if(std::isspace(givenTextEasy[i]) && std::isspace(userText[j]))    
+                    continue;
             }
             else 
-                if(*it == ' ' || *it == '\n' || *it == '\t')
-                    state = out;
+                continue;
         }
-        return count;
+        return score;
     }
 
     void startSpeed(){
@@ -87,15 +77,12 @@ public:
             if(startSpeed != givenTime) //add if a substring is equal too
                 countScore++; 
             else
-                return;
-        
-        
-        
+                return;        
     }
     void checkSpeed(std::string text) {
     
         std::cout << name << ", please start typing whenever you feel like it: \n";
-        std::cin >> this -> text;
+        std::cin >> userText;
 
          //if user got one word right, FIX this later
             if(score >= 20 && score < 50){
@@ -126,33 +113,11 @@ public:
 
 int main(){
     std::string name;
-    int score = 50;
-
-    /*std::string text = "Remember when you were young, you shone like the sun\
-                        Shine on you crazy diamond\
-                        Now there's a look in your eyes, like black holes in the sky\
-                        Shine on you crazy diamond\
-                        You were caught on the crossfire of childhood and stardom\
-                        Blown on the steel breeze\
-                        Come on you target for faraway laughter\
-                        Come on you stranger, you legend, you martyr, and shine\
-                        You reached for the secret too soon, you cried for the moon\
-                        Shine on you crazy diamond\
-                        Threatened by shadows at night, and exposed in the light\
-                        Shine on you crazy diamond\
-                        Well you wore out your welcome with random precision\
-                        Rode on the steel breeze\
-                        Come on you raver, you seer of visions\
-                        Come on you painter, you piper, you prisoner, and shine";
-    */
-    std::string text = "No you dumbass";
     std::cout << "Input your name, master\n";
-    std::cin >> name;
-    Speed obj(name, score);
-    //obj.checkSpeed(text);   
-    std::cout << obj.countSubstring(text);
+    std::getline(std::cin, name);
+       
+    Speed obj(name);   
+    std::cout << obj.countSubstring();
     /*Whole system of a down(ha ha ha), system of scoring, could be put in function later*/
-    
-
     return 0;
 }
